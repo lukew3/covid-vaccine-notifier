8e3fec31ad9d014d38df6d22691b79229eb7fd41
lukew3 <lukew25073@gmail.com>
2021-03-23 20:15:42 -0400

    added environment variables, in range location count

--- monitor.py
+++ monitor.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
-import time, requests, smtplib, math
+import time, requests, smtplib, math, json
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
-gmail_user = 'covidvaccinespotterscraper@gmail.com'
-gmail_password = 'wzmiwytqdrvdobrc'
+gmail_user = config.get('GMAIL_USER')
+gmail_password = config.get('GMAIL_PASSWORD')
 
 server.login(gmail_user, gmail_password)
 
@@ -30,27 +33,30 @@ def check_appointments():
     active_users = client.vaccineNotifier.users.find({ "active": True })
     for user in active_users:
         accepted_appointments = []
+        in_range_locations = 0
         for appointment in states_dict[ user["state"] ]["features"]:
             apt_coords = appointment["geometry"]["coordinates"]
             try:
                 dist = round(coords_distance(apt_coords, user["coords"]), 2)
-                if ((dist <= user["distance"]) and (appointment["properties"]["appointments_available"])):
-                    props = appointment["properties"]
-                    full_address = f"{props['address']}, {props['city']}, {state}"
-                    apt_dict = { "address":full_address, "distance":dist, "url":props["url"], "name":props["name"] }
-                    accepted_appointments.append(apt_dict)
+                if (dist <= user["distance"]):
+                    in_range_locations += 1
+                    if (appointment["properties"]["appointments_available"]):
+                        props = appointment["properties"]
+                        full_address = f"{props['address']}, {props['city']}, {state}"
+                        apt_dict = { "address":full_address, "distance":dist, "url":props["url"], "name":props["name"] }
+                        accepted_appointments.append(apt_dict)
             except Exception as e:
                 pass
         accepted_appointments = sorted(accepted_appointments, key=lambda x: x["distance"])
-        send_email(user["email"], accepted_appointments)
+        send_email(user["email"], accepted_appointments, in_range_locations)
 
 
-def send_email(recipient, appointments):
-    mail_content = "Appointments found at: \n"
+def send_email(recipient, appointments, in_range_locations):
+    mail_content = f"{len(appointments)}/{in_range_locations} locations available. \nAppointments found at: \n"
     for appointment in appointments:
         mail_content += appointment["name"] + ", " + \
                     appointment["address"] + ". " + str(appointment["distance"]) + " miles away." + \
-                    "click the link to schedule: " + appointment["url"] + "\n"
+                    " Click the link to schedule: " + appointment["url"] + "\n"
     message = MIMEMultipart()
     message['From'] = gmail_user
     message['To'] = recipient
